56e226396b2a1bc3ad9b072f3db57158ec1e4374
endstoff <63933131+endstoff@users.noreply.github.com>
2021-03-12 18:29:22 +0100

    mapping

---
+++ server-data/resources/[mapping]/SLBK11_MissionRow/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+client_script {
+    "stream/main.lua"
+}
+
+files {
+    "interiorproxies.meta"
+}
+
+
+
+
+client_script "21286.lua"
