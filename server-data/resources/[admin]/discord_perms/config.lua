2538022661123830bc419247f0aaa8570d5095f4
endstoff <63933131+endstoff@users.noreply.github.com>
2021-03-15 00:13:21 +0100

    tik tok on se clock

--- server-data/resources/[admin]/discord_perms/config.lua
+++
@@ -1,9 +0,0 @@
-Config = {
-	DiscordToken = "NzkyNDAwNzc3ODg5MDU0NzQw.X-dKuw.9xDJ_k_psoxJEMpvJY1CX3qQ21w",
-	GuildId = "792524972262162482",
-
-	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
-	Roles = {
-		["792530615513251841"] = "Verifiziert | Legacy-V" -- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
-	}
-}
